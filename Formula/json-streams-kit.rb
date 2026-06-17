a62b0862e3a219bdf8a61fb901a5a6c86171e0b8
wdonne <werner.donne@pincette.biz>
2026-06-17 19:48:00 +0200

    json-streams-kit 3.1.5

--- Formula/json-streams-kit.rb
+++ Formula/json-streams-kit.rb
@@ -1,11 +1,11 @@
-# Generated with JReleaser 1.24.0 at 2026-06-17T19:46:22.614598+02:00
+# Generated with JReleaser 1.24.0 at 2026-06-17T19:47:58.832675+02:00
 
 class JsonStreamsKit < Formula
   desc "JSON Streams utilities"
   homepage "https://jsonstreams.io"
   url "https://github.com/json-event-sourcing/pincette-jes-cli/releases/download/3.1.5/pincette-jes-cli-3.1.5-jar-with-dependencies.jar", :using => :nounzip
   version "3.1.5"
-  sha256 "83b06c021d37acb2a5bc49a0cb1d6996d2301b82ef73b828357600afb71c3913"
+  sha256 "4af314670e4090f52245611913896003d71222e11d6ec8cfa57576983affba0c"
   license "BSD-2-Clause"
 
   depends_on "openjdk@21"
