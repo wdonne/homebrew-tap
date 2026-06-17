cd9f523c8438afd7a9d79ca14a7659e7b12642d8
wdonne <werner.donne@pincette.biz>
2026-06-17 16:59:56 +0200

    json-streams 2.9.1

--- Formula/json-streams.rb
+++ Formula/json-streams.rb
@@ -1,30 +1,30 @@
-# Generated with JReleaser 1.24.0 at 2026-05-23T21:47:52.934795+02:00
+# Generated with JReleaser 1.24.0 at 2026-06-17T16:59:54.130665+02:00
 
 class JsonStreams < Formula
   desc "JSON Streams"
   homepage "https://jsonstreams.io"
-  url "https://github.com/json-event-sourcing/pincette-json-streams/releases/download/2.9.0/pincette-json-streams-2.9.0-jar-with-dependencies.jar", :using => :nounzip
-  version "2.9.0"
-  sha256 "f8710f21f22270c829ebe1e9c420925e1f04f1c60693520fb1184a316821f0e4"
+  url "https://github.com/json-event-sourcing/pincette-json-streams/releases/download/2.9.1/pincette-json-streams-2.9.1-jar-with-dependencies.jar", :using => :nounzip
+  version "2.9.1"
+  sha256 "9978ad10d29dcb0f3881131540608eb140341d7c4beb1e97402b755d9bd73f96"
   license "BSD-2-Clause"
 
   depends_on "openjdk@21"
 
   def install
-    libexec.install "pincette-json-streams-2.9.0-jar-with-dependencies.jar"
+    libexec.install "pincette-json-streams-2.9.1-jar-with-dependencies.jar"
 
     bin.mkpath
     File.open("#{bin}/json-streams", "w") do |f|
       f.write <<~EOS
         #!/bin/bash
         export JAVA_HOME="#{Language::Java.overridable_java_home_env(nil)[:JAVA_HOME]}"
-        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/pincette-json-streams-2.9.0-jar-with-dependencies.jar "$@"
+        exec "${JAVA_HOME}/bin/java" -jar #{libexec}/pincette-json-streams-2.9.1-jar-with-dependencies.jar "$@"
       EOS
     end
   end
 
   test do
     output = shell_output("#{bin}/json-streams --version")
-    assert_match "2.9.0", output
+    assert_match "2.9.1", output
   end
 end
