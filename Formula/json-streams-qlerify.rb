099783ce91bbbaa844496db192d37b6d52e690f2
wdonne <werner.donne@pincette.biz>
2026-06-27 17:08:40 +0200

    json-streams-qlerify 0.1.1

--- Formula/json-streams-qlerify.rb
+++ Formula/json-streams-qlerify.rb
@@ -1,26 +1,26 @@
-# Generated with JReleaser 1.24.0 at 2026-06-26T23:26:05.407877+02:00
+# Generated with JReleaser 1.24.0 at 2026-06-27T17:08:38.204955+02:00
 
 class JsonStreamsQlerify < Formula
   desc "Generate JSON Streams apps and OpenAPI specs from Qlerify"
   homepage "https://github.com/wdonne/json-streams-qlerify"
-  version "0.1.0"
+  version "0.1.1"
   license "BSD-2-Clause"
 
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.0/json-streams-qlerify-0.1.0-aarch64-unknown-linux-musl.zip"
-    sha256 "7a34c9154f44addcbb2b14811bf070d6a5fc40f2bf8d5bc9969f9c76590f5538"
+    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.1/json-streams-qlerify-0.1.1-aarch64-unknown-linux-musl.zip"
+    sha256 "576c43e6135b362227d25e98ea475e7619ba9074381dba4e4af8f2c9c15df8c6"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.0/json-streams-qlerify-0.1.0-x86_64-unknown-linux-musl.zip"
-    sha256 "eff4d9a9f8710c183284d1f18450e2df73ff13dcb5a15b2cc212b22d9ad0fb6f"
+    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.1/json-streams-qlerify-0.1.1-x86_64-unknown-linux-musl.zip"
+    sha256 "68b40a709feb9888b606a11a133f59b2e8c9ad8729f163bf176313e2ba4b9ae1"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.0/json-streams-qlerify-0.1.0-aarch64-apple-darwin.zip"
-    sha256 "90fbe6be62d3988502c0ab9507bf87d77248b12239df930658a5aa5008f0bd88"
+    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.1/json-streams-qlerify-0.1.1-aarch64-apple-darwin.zip"
+    sha256 "435cbba57059739ebe14386d127374180a30f6676f8009947ff706d374636c6d"
   end
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.0/json-streams-qlerify-0.1.0-x86_64-apple-darwin.zip"
-    sha256 "8f11b2602b6908c41bce079d242d2b67ac34acef2306d1ae85a6f93e785706dc"
+    url "https://github.com/wdonne/json-streams-qlerify/releases/download/0.1.1/json-streams-qlerify-0.1.1-x86_64-apple-darwin.zip"
+    sha256 "440219f93c3e3aa2d4a0c443fc77b86d87d517bd00c3785f994994e00e89bb26"
   end
 
 
@@ -31,6 +31,6 @@ class JsonStreamsQlerify < Formula
 
   test do
     output = shell_output("#{bin}/json-streams-qlerify --version")
-    assert_match "0.1.0", output
+    assert_match "0.1.1", output
   end
 end
